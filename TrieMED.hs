b0729de0e0e9ea8f537ba6292d66c091148e1e48
Sebastian J. Mielke <sjm@sjmmusic.de>
2014-06-19 16:45:33 +0200

    API changes. Lots of performance tests. Nothing works. Brute force it is.

--- TrieMED.hs
+++ TrieMED.hs
@@ -1,4 +1,4 @@
-module Spell where
+module TrieMED where
 
 import Data.List (sort, sortBy, nub)
 import Data.Ord (comparing)
@@ -6,47 +6,58 @@ import Data.Maybe (maybeToList)
 
 import WTrie
 
+-- | A MED
 type MEDTable = [(Char, Int)]
 type Result = (String, Int)
 
 -- | A MEDTableState consists of a (one column) table for MED calculationy,
--- the current word the MED is calculated for (careful though, the word is reversed!)
--- and the current best guess new guesses would have to be better than.
-type MEDTableState = (MEDTable, String, Int)
+-- the current word the MED is calculated for (careful though, the word is reversed!).
+type MEDTableState = (MEDTable, String)
 
-calcMED :: String -> [WTrie] -> [Result]
-calcMED uword ts = tenBest
-                 $ map (\(s,i) -> (reverse s, i))
-                 $ reduceTries (initialState uword, []) ts
+calcMEDs :: String -> WTrie -> [Result]
+calcMEDs uword ts = tenBest
+--                  $ (\l -> [(uword ++ " compared against #nodes", length l)])
+                  $ map (\(s,i) -> (reverse s, i))
+                  $ reduceTriesAll (initialState uword) ts
+--                  $ reduceTriesKeepTrack (initialState uword, []) ts
 
 tenBest :: Ord o => [(a, o)] -> [(a, o)]
 tenBest = take 10 . sortBy (comparing snd)
 
 initialState :: String -> MEDTableState
-initialState s = (zip (' ':s) [0..], "", maxBound)
+initialState s = (zip (' ':s) [0..], "")
 
-reduceTries :: (MEDTableState, [Result]) -> [WTrie] -> [Result]
-reduceTries _ [] = []
-reduceTries (oldState, resultsSoFar) (t:ts) = let newResList = tenBest $ reduceIndiv t
-                                              in newResList ++ reduceTries (oldState, newResList) ts
+reduceTriesAll :: MEDTableState -> WTrie -> [Result]
+reduceTriesAll (oldState) ts = concatMap reduceIndiv $ ts
     where
-        reduceIndiv :: WTrie -> [Result]
-        reduceIndiv (WNode l f cs) = let (newState@(currentTable, currentWord, _), res, currentMED) = calcNode oldState l f
+        reduceIndiv :: WIndivTrie -> [Result]
+        reduceIndiv (WNode l f cs) = let (newState@(currentTable, currentWord), res, currentMED) = calcNode oldState l f
+                                     in case res of Nothing ->     reduceTriesAll (newState) cs
+                                                    Just r  -> r : reduceTriesAll (newState) cs
+
+reduceTriesKeepTrack :: (MEDTableState, [Result]) -> WTrie -> [Result]
+reduceTriesKeepTrack _ [] = []
+reduceTriesKeepTrack (oldState, resultsSoFar) (t:ts) = let newResList = reduceIndiv t
+                                                       in newResList ++ reduceTriesKeepTrack (oldState, newResList) ts
+    where
+        reduceIndiv :: WIndivTrie -> [Result]
+        reduceIndiv (WNode l f cs) = let (newState@(currentTable, currentWord), res, currentMED) = calcNode oldState l f
                                          -- The best MED that is still possible in future steps is MED - max(0, wordLengthDifference)
                                          bestMEDStillPossible = currentMED - max 0 ((length currentTable - 1) - (length currentWord))
                                          newResultList = case res of Nothing -> resultsSoFar
-                                                                     Just r  -> tenBest $ updateResList r resultsSoFar
+                                                                     Just r  -> updateResList r resultsSoFar
                                          followers = if length newResultList < 10
-                                                     || bestMEDStillPossible < maximum (map snd newResultList) -- maximum won't fail. Short-circuit logic, bitches.
-                                                     then reduceTries (newState, newResultList) cs
+                                                     || bestMEDStillPossible <= (last $ take 10 $ sort $ (map snd newResultList)) -- last won't fail. Short-circuit logic, bitches.
+                                                     then reduceTriesKeepTrack (newState, newResultList) cs
                                                      else []
                                      in case res of Nothing ->     followers
                                                     Just r  -> r : followers
         updateResList r [] = [r]
         updateResList r l@(x:xs) = if snd r <= snd x then r : l else x : updateResList r xs
 
+
 calcNode :: MEDTableState -> Char -> Bool -> (MEDTableState, Maybe Result, Int)
-calcNode (oldTable, currentWord, currentBest) l f = ((newTable, l:currentWord, currentBest), newRes, newMED)
+calcNode (oldTable, currentWord) l f = ((newTable, l:currentWord), newRes, newMED)
     where
         -- previous column and the value below the current one
         step :: MEDTable -> Int -> MEDTable
--- WTrie.hs
+++ WTrie.hs
@@ -1,13 +1,27 @@
-{-# LANGUAGE BangPatterns #-}
-
-module WTrie (WTrie(..), toList, fromFile, fromList) where
+module WTrie (
+    -- * Data type
+    WIndivTrie(..),
+    WTrie,
+    
+    -- * Generation from and to lists
+    toList,
+    fromList,
+    fromFile,
+    
+    -- * Helper functions
+    contains
+    
+    ) where
 
 import Data.List (intersperse)
 
+-- | This Trie implementation uses one @WNode@ per @Char@.
+-- Such a node can have many following characters and be the end of a word or not.
+data WIndivTrie = WNode { letter :: Char, final :: Bool, children :: [WIndivTrie] }
+-- | Since a @WIndivTrie@ starts with the first letter, a list is needed for a word list representation.
+type WTrie = [WIndivTrie]
 
-data WTrie = WNode { letter :: Char, final :: Bool, children :: [WTrie] }
-
-instance Show WTrie where
+instance Show WIndivTrie where
     show = walk 1
         where
             walk indent (WNode l f cs) = (if f then "+-([" ++ [l] ++ "]) "
@@ -23,25 +37,41 @@ instance Show WTrie where
             applyTail f xs = head xs : map f (tail xs)
     showList ts = (++) $ concat . intersperse "\n" $ map show ts
 
-singleton :: String -> [WTrie]
+-- | Creates a (simply linear) @WTrie@ from one word.
+singleton :: String -> WTrie
 singleton (c:[]) = [WNode c True  []            ]
 singleton (c:cs) = [WNode c False (singleton cs)]
 
-addToTrie :: String -> [WTrie] -> [WTrie]
+-- | Adds a word to an existing @WTrie@.
+addToTrie :: String -> WTrie -> WTrie
 addToTrie s [] = singleton s
-addToTrie (x:[]) ((t@(WNode l f cs)):ts) = if x == l then t{final = True} : ts
-                                                     else t               : addToTrie (x:[]) ts
-addToTrie (x:xs) ((t@(WNode l f cs)):ts) = if x == l then t{children = addToTrie xs cs} : ts
-                                                     else t                             : addToTrie (x:xs) ts
+addToTrie (x:[]) ((t@(WNode l f cs)):ts) = if x == l
+                                           then t{final = True} : ts
+                                           else t               : addToTrie (x:[]) ts
+addToTrie (x:xs) ((t@(WNode l f cs)):ts) = if x == l
+                                           then t{children = addToTrie xs cs} : ts
+                                           else t                             : addToTrie (x:xs) ts
 
-fromFile :: String -> IO [WTrie]
+
+-- | Loads a whitespace-separated word list and creates a @WTrie@ out of all words.
+fromFile :: String -> IO WTrie
 fromFile f = do rawwords <- readFile f
                 return $ fromList . words $ rawwords
 
-
-fromList :: [String] -> [WTrie]
+-- | Creates a @WTrie@ from a word list.
+fromList :: [String] -> WTrie
 fromList = foldr addToTrie []
 
-toList :: [WTrie] -> [String]
+-- | Recreates a list of all words contained in a @WTrie@.
+toList :: WTrie -> [String]
 toList = concatMap singleToList
     where singleToList (WNode l f cs) = map (l:) $ (if f then ("":) else id) $ toList cs
+
+-- | Checks whether a @WTrie@ contains a word.
+contains :: WTrie -> String -> Bool
+contains _ "" = True
+contains [] _ = False
+contains ((t@(WNode l f cs)):ts) s@(x:[]) = if l == x && f then True else False
+contains ((t@(WNode l f cs)):ts) s@(x:xs) = if l == x
+                                            then contains cs xs
+                                            else contains ts s
