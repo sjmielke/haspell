c3cd03b443eee1cb432201048dd0cd227399547a
Sebastian J. Mielke <sjm@sjmmusic.de>
2014-06-23 17:18:51 +0200

    Simple console UI works perfectly. Some more features will have to be implemented, though.

--- Haspell.hs
+++ Haspell.hs
@@ -1,10 +1,13 @@
-module Haspell where
+module Main where
 
 import Data.Default (def)
 import Data.Char (isLetter)
 import Data.List.Split (split, whenElt, dropBlanks, keepDelimsR, onSublist)
+import Text.Read (readMaybe)
+import System.Exit (exitSuccess)
 
 import WTrie
+import TrieMED
 
 -- | Wrap text with given ANSI code and reset code to change display properties.
 -- (see @http://en.wikipedia.org/wiki/ANSI_escape_code@ )
@@ -24,7 +27,7 @@ type Sentence = [SentencePart]
 
 -- | Callable entry point for interactive text correction.
 correctText :: String -> WTrie -> IO String
-correctText txt ts = do newSentences <- correctSentences (segmentText txt) ts
+correctText txt ts = do newSentences <- mapM (correctSentence ts) (segmentText txt)
                         return $ renderSentences newSentences
 
 -- | Primitive segmentation of a given user input into 'Sentence's
@@ -41,9 +44,40 @@ segmentText = map segmentSentence . splitOnSubSeqs [". ", "! ", "? "] . (:[])
         segmentSentence = map (\s@(x:xs) -> if isLetter x then Word s else Punctuation s)
                         . split (dropBlanks $ whenElt (not . isLetter))
 
--- | Interactively correct a segmented text using a given 'WTrie'.
-correctSentences :: [Sentence] -> WTrie -> IO [Sentence]
-correctSentences ss ts = return ss
+-- | Interactively correct a segmented sentence using a given 'WTrie'.
+correctSentence :: WTrie -> Sentence -> IO Sentence
+correctSentence ts s = mapM correctSentencePart sWithIds
+    where
+        sWithIds = zip [0..] s
+        correctSentencePart :: (Int, SentencePart) -> IO SentencePart
+        correctSentencePart (i, p@(Punctuation _)) = return p
+        correctSentencePart (i, Word w) = do if ts `contains` w
+                                             then return $ Word w
+                                             else do
+                                                  let results = calcMEDs w ts
+                                                      errorCase = do putStrLn $ invertText "What the fuck is wrong with you? Try again."
+                                                                     correctSentencePart (i, Word w)
+                                                  putStrLn $ highlightInSentence i sWithIds
+                                                  putStrLn $ renderAsTable results
+                                                  rawchoice <- getLine
+                                                  case readMaybe rawchoice of
+                                                      Nothing -> case rawchoice of
+                                                                     "i" -> return $ Word w
+                                                                     "m" -> errorCase -- TODO
+                                                                     "x" -> exitSuccess
+                                                      Just ch -> do if ch >= 0 && ch < length results
+                                                                    then return $ Word (fst $ results !! ch)
+                                                                    else errorCase
+        highlightInSentence :: Int -> [(Int, SentencePart)] -> String
+        highlightInSentence i = ('\n':) . concatMap (\(i', p) -> case p of Punctuation w' -> faintText w'
+                                                                           Word w'        -> if i /= i' then faintText w' else w')
+        renderAsTable :: [Result] -> String
+        renderAsTable = ("\n" ++)
+                      . (++ faintText "i ignore\nm more suggestions\nx exit/abort")
+                      . concatMap (\(i, (s, c)) -> faintText (show i ++ ". ")
+                                                ++ s
+                                                ++ faintText (" (" ++ show c ++ ")\n"))
+                      . zip [0..]
 
 -- | Undo 'segmentText' and restore a 'Sentence' list into plain text.
 renderSentences :: [Sentence] -> String
@@ -51,41 +85,7 @@ renderSentences = foldr (\s acc -> concatMap renderSentencePart s ++ acc) ""
     where renderSentencePart p = case p of Word s -> s
                                            Punctuation s -> s
 
-{- -- Old inflexible approach to separating words.
-type UserText = (String, String, String)
-
-correctText :: WTrie -> String -> IO String
-correctText ts txt = do (p, w, n) <- walkWords $ splitFirstWord ("", "", txt)
-                        return $ p ++ w ++ n
-    where
-        walkWords :: UserText -> IO UserText
-        walkWords u = do nu@(p, w, n) <- processWord u
-                         if null w then return nu else walkWords nu
-        processWord :: UserText -> IO (UserText)
-        processWord t@(prevText, userWord, nextText) = do let (nPrev, nWord, nNext) = splitFirstWord ("", "", nextText)
-                                                          -- putStrLn . show $ t
-                                                          putStrLn $ faintText (takeFromEnd 10 prevText) ++ userWord ++ faintText (take 10 nextText)
-                                                          correctedWord <- return userWord
-                                                          return (prevText ++ correctedWord ++ nPrev, nWord, nNext)
-        splitFirstWord :: UserText -> UserText
-        splitFirstWord t@(_, _, []) = t
-        splitFirstWord (p, w, (n:ns)) = if isLetter n
-                                        then splitFirstWord (p, w ++ [n], ns)
-                                        else if null w
-                                             then splitFirstWord (p ++ [n], w, ns)
-                                             else (p, w, (n:ns))
-        takeFromEnd i = reverse . take i . reverse
--}
-
-{- -- Later maybe.
-import Graphics.Vty
-main = do vty <- mkVty def
-          let line0 = string (def `withForeColor` green) "Foo"
-              line1 = string (def `withForeColor` yellow) "Bar"
-              img = line0 <|> line1
-              pic = picForImage img
-          update vty pic
-          e <- nextEvent vty
-          shutdown vty
-          print $ "Last event was: " ++ show (e :: Event)
--}
+main = do ts <- fromWLFile "/home/sjm/downloads/aspell-dump-expand-de_DE.utf8.txt"
+          putStrLn $ (show length $ toList ts) ++ " words loaded." -- Cheap deepseq. Also nice to know.
+          result <- correctText "- ... das ist (natürlich) ein Test, welcher Dinge? (Naja...) tun will...! hachja" ts
+          putStrLn $ "RESULT: " ++ result
