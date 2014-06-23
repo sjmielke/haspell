91efc7c0a7191c6e0d49c4053712155894004ccf
Sebastian J. Mielke <sjm@sjmmusic.de>
2014-06-23 15:51:07 +0200

    UI work. Simple text and sentence deconstruction to prepare the correction of indiviual words.

--- Haspell.hs
+++ Haspell.hs
@@ -1,7 +1,13 @@
 module Haspell where
 
+import Data.Default (def)
+import Data.Char (isLetter)
+import Data.List.Split (split, whenElt, dropBlanks, keepDelimsR, onSublist)
+
+import WTrie
+
 -- | Wrap text with given ANSI code and reset code to change display properties.
--- (see http://en.wikipedia.org/wiki/ANSI_escape_code )
+-- (see @http://en.wikipedia.org/wiki/ANSI_escape_code@ )
 ansiWrap :: Int -> String -> String
 ansiWrap i = (("\ESC[" ++ (show i) ++ "m")++) . (++"\ESC[0m")
 
@@ -12,3 +18,74 @@ invertText = ansiWrap 7
 -- | Insert ANSI codes to display the argument in fainter font.
 faintText :: String -> String
 faintText = ansiWrap 2
+
+data SentencePart = Word String | Punctuation String deriving (Show)
+type Sentence = [SentencePart]
+
+-- | Callable entry point for interactive text correction.
+correctText :: String -> WTrie -> IO String
+correctText txt ts = do newSentences <- correctSentences (segmentText txt) ts
+                        return $ renderSentences newSentences
+
+-- | Primitive segmentation of a given user input into 'Sentence's
+-- containing 'SentencePart's ('Word's and 'Punctionation')
+segmentText :: String -> [Sentence]
+segmentText = map segmentSentence . splitOnSubSeqs [". ", "! ", "? "] . (:[])
+    where
+        splitOnSubSeqs :: [String] -> [String] -> [String]
+        splitOnSubSeqs [] text = text
+        splitOnSubSeqs (sq:sqs) text = concat
+                                     $ map (split (keepDelimsR $ dropBlanks $ onSublist sq))
+                                     $ splitOnSubSeqs sqs text
+        segmentSentence :: String -> Sentence
+        segmentSentence = map (\s@(x:xs) -> if isLetter x then Word s else Punctuation s)
+                        . split (dropBlanks $ whenElt (not . isLetter))
+
+-- | Interactively correct a segmented text using a given 'WTrie'.
+correctSentences :: [Sentence] -> WTrie -> IO [Sentence]
+correctSentences ss ts = return ss
+
+-- | Undo 'segmentText' and restore a 'Sentence' list into plain text.
+renderSentences :: [Sentence] -> String
+renderSentences = foldr (\s acc -> concatMap renderSentencePart s ++ acc) ""
+    where renderSentencePart p = case p of Word s -> s
+                                           Punctuation s -> s
+
+{- -- Old inflexible approach to separating words.
+type UserText = (String, String, String)
+
+correctText :: WTrie -> String -> IO String
+correctText ts txt = do (p, w, n) <- walkWords $ splitFirstWord ("", "", txt)
+                        return $ p ++ w ++ n
+    where
+        walkWords :: UserText -> IO UserText
+        walkWords u = do nu@(p, w, n) <- processWord u
+                         if null w then return nu else walkWords nu
+        processWord :: UserText -> IO (UserText)
+        processWord t@(prevText, userWord, nextText) = do let (nPrev, nWord, nNext) = splitFirstWord ("", "", nextText)
+                                                          -- putStrLn . show $ t
+                                                          putStrLn $ faintText (takeFromEnd 10 prevText) ++ userWord ++ faintText (take 10 nextText)
+                                                          correctedWord <- return userWord
+                                                          return (prevText ++ correctedWord ++ nPrev, nWord, nNext)
+        splitFirstWord :: UserText -> UserText
+        splitFirstWord t@(_, _, []) = t
+        splitFirstWord (p, w, (n:ns)) = if isLetter n
+                                        then splitFirstWord (p, w ++ [n], ns)
+                                        else if null w
+                                             then splitFirstWord (p ++ [n], w, ns)
+                                             else (p, w, (n:ns))
+        takeFromEnd i = reverse . take i . reverse
+-}
+
+{- -- Later maybe.
+import Graphics.Vty
+main = do vty <- mkVty def
+          let line0 = string (def `withForeColor` green) "Foo"
+              line1 = string (def `withForeColor` yellow) "Bar"
+              img = line0 <|> line1
+              pic = picForImage img
+          update vty pic
+          e <- nextEvent vty
+          shutdown vty
+          print $ "Last event was: " ++ show (e :: Event)
+-}
--- WTrie.hs
+++ WTrie.hs
@@ -5,10 +5,13 @@ module WTrie (
     WIndivTrie(..),
     WTrie,
     
-    -- * Generation from and to lists
-    toList,
+    -- * Generation from and into different representations
     fromList,
-    fromFile,
+    toList,
+    fromWLFile,
+    toWLFile,
+    fromWTFile,
+    toWTFile,
     
     -- * Helper functions
     contains
@@ -17,34 +20,34 @@ module WTrie (
 
 import Data.List (intersperse)
 
--- | This Trie implementation uses one @WNode@ per @Char@.
+-- | This Trie implementation uses one 'WNode' per 'Char'.
 -- Such a node can have many following characters and be the end of a word or not.
-data WIndivTrie = WNode { letter :: Char, final :: Bool, children :: [WIndivTrie] }
--- | Since a @WIndivTrie@ starts with the first letter, a list is needed for a word list representation.
+data WIndivTrie = WNode { letter :: Char, final :: Bool, children :: [WIndivTrie] } deriving (Show, Read)
+-- | Since a 'WIndivTrie' starts with the first letter, a list is needed for a word list representation.
 type WTrie = [WIndivTrie]
 
-instance Show WIndivTrie where
-    show = walk 1
-        where
-            walk indent (WNode l f cs) = (if f then "+-([" ++ [l] ++ "]) "
-                                               else "+--(" ++ [l] ++ ")--" )
-                                         ++ ( concat
-                                            . intersperse "\n"
-                                            . applyTail ( ((++) $ concat $ replicate indent "|       ")
-                                                        . ("\\"++) . tail)
-                                            . map (walk $ indent + 1)
-                                            $ cs)
-            applyTail :: (a -> a) -> [a] -> [a]
-            applyTail _ [] = []
-            applyTail f xs = head xs : map f (tail xs)
-    showList ts = (++) $ concat . intersperse "\n" $ map show ts
+-- | Simple prettyprinting function for 'WTrie's.
+prettyprint :: WTrie -> String
+prettyprint ts = concat . intersperse "\n" $ map (ppIndivWalk 1) ts
+    where
+        ppIndivWalk indent (WNode l f cs) = (if f then "+-([" ++ [l] ++ "]) "
+                                                  else "+--(" ++ [l] ++ ")--" )
+                                            ++ ( concat
+                                               . intersperse "\n"
+                                               . applyToTail ( ((++) $ concat $ replicate indent "|       ")
+                                                             . ("\\"++) . tail)
+                                               . map (ppIndivWalk $ indent + 1)
+                                               $ cs)
+        applyToTail :: (a -> a) -> [a] -> [a]
+        applyToTail _ [] = []
+        applyToTail f xs = head xs : map f (tail xs)
 
--- | Creates a (simply linear) @WTrie@ from one word.
+-- | Creates a (simply linear) 'WTrie' from one word.
 singleton :: String -> WTrie
 singleton (c:[]) = [WNode c True  []            ]
 singleton (c:cs) = [WNode c False (singleton cs)]
 
--- | Adds a word to an existing @WTrie@.
+-- | Adds a word to an existing 'WTrie'.
 addToTrie :: String -> WTrie -> WTrie
 addToTrie s [] = singleton s
 addToTrie (x:[]) ((t@(WNode l f cs)):ts) = if x == l
@@ -54,21 +57,34 @@ addToTrie (x:xs) ((t@(WNode l f cs)):ts) = if x == l
                                            then t{children = addToTrie xs cs} : ts
                                            else t                             : addToTrie (x:xs) ts
 
--- | Loads a whitespace-separated word list and creates a @WTrie@ out of all words.
-fromFile :: String -> IO WTrie
-fromFile f = do rawwords <- readFile f
-                return $ fromList . words $ rawwords
+-- | Loads a whitespace-separated word list and creates a 'WTrie' out of all words.
+fromWLFile :: FilePath -> IO WTrie
+fromWLFile f = do rawwords <- readFile f
+                  return $ fromList . words $ rawwords
 
--- | Creates a @WTrie@ from a word list.
+-- | Write the word list contained in a  'WTrie' into a file.
+toWLFile :: FilePath -> WTrie -> IO ()
+toWLFile f ts = writeFile f $ concat . intersperse "\n" $ toList ts
+
+-- | Loads a Serialization of a 'WTrie' that was created using the show Instance.
+fromWTFile :: FilePath -> IO WTrie
+fromWTFile f = do rawtrie <- readFile f
+                  return $ read rawtrie
+
+-- | Write a 'WTrie' into a file using the show-Serialization.
+toWTFile :: FilePath -> WTrie -> IO ()
+toWTFile f ts = writeFile f (show ts)
+
+-- | Creates a 'WTrie' from a word list.
 fromList :: [String] -> WTrie
 fromList = foldr addToTrie []
 
--- | Recreates a list of all words contained in a @WTrie@.
+-- | Recreates a list of all words contained in a 'WTrie'.
 toList :: WTrie -> [String]
 toList = concatMap singleToList
     where singleToList (WNode l f cs) = map (l:) $ (if f then ("":) else id) $ toList cs
 
--- | Checks whether a @WTrie@ contains a word.
+-- | Checks whether a 'WTrie' contains a word.
 contains :: WTrie -> String -> Bool
 contains _ "" = True
 contains [] _ = False
